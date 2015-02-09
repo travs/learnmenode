095aa2abd17fc055ba652a2e6c71fb62ad02beb5
Travis Jacobs <trav221@gmail.com>
2015-02-09 00:02:23 -0330

    Complete lesson 9

--- lesson9/juggling_async.js
+++ lesson9/juggling_async.js
@@ -1,25 +1,26 @@
+
 var http = require('http');
+var url = require('url');
 
-var completed = 0;
-var queue = [];
+var completed = 0;          //how many 'end' responses have we?
+var holder = Object();      //holds hostnames:strings
 for(var x = 0; x < process.argv.length - 2; x++){
-  getit(process.argv[x+2], x);
-}
-
-function getit(url){
-  var pos = queue.length;
-  queue[pos] = pos;
-  http.get(url, callback);
+  address = process.argv[x+2];
+  hostname = address.split(':')[2];
+  holder[hostname] = '';       //initialize hostname key w/ empty string
+  http.get(address, callback);     //start the request
 }
 
 function callback(response){
   response.setEncoding('utf8');
   response.on('data', function(data){
-    queue[pos] += data;
+    hostname = url.parse(this.req._headers.host).hostname;
+    //reverse-engineered hostname. Maybe there's a better way to get this?
+    holder[hostname] += data;
   })
   response.on('end', function(){
     completed += 1;
-    if(completed == 3){
+    if(completed === 3){
       printem();
     }
   })
@@ -29,7 +30,7 @@ function callback(response){
 }
 
 function printem(){
-  for(var x in queue){
-    console.log(queue[x]);
+  for(x in holder){
+    console.log(holder[x]);
   }
 }
